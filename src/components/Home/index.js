9fc135c7f89ec11e3bd99d534d6a60a16435ed4b
Venkatshivasai <venkatshivasai10@gmail.com>
2023-11-17 10:25:50 +0000

    fetching

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+import BlogList from '../BlogList'
+
+import UserInfo from '../UserInfo'
+
+const Home = () => (
+  <div className="home-container">Render UserInfo and BlogList</div>
+)
+
+export default Home
